d405cc69537940f9d662ccc0d9b234b2baae3e82
Duncan <buzzbuzz.macdonald@gmail.com>
2017-09-07 17:32:00 -0700

    python

---
+++ functions.py
@@ -0,0 +1,19 @@
+# Solution for problem 8
+
+
+def fibonacci_iterative(num = 10):
+    previous = 1
+    precedingPrevious = 0
+    for number in range(0,num):
+        fibonacci = (previous + precedingPrevious)
+        print(fibonacci)
+        precedingPrevious = previous
+        previous = fibonacci
+
+
+# Solution for problem 9
+# I don't know how to call stuff from the terminal so I'm just doing an input in the program
+
+arg = int(input("Enter a number: "))
+
+fibonacci_iterative(arg)
